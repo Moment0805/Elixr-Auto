e48f9176ae9350f0138cfa22ae6175d83146ee2d
emmanuelokedele-dev <emmanuel.okedele@chronosng.com>
2025-10-27 12:50:24 +0100

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './components/pages/HomePage';
@@ -11,22 +12,24 @@ import CryptoPage from './components/pages/CryptoPage';
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white flex flex-col">
-        <Header />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/listings" element={<ListingsPage />} />
-            <Route path="/dealer-signup" element={<DealerSignupPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/crypto" element={<CryptoPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <div className="min-h-screen bg-white flex flex-col">
+          <Header />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/listings" element={<ListingsPage />} />
+              <Route path="/dealer-signup" element={<DealerSignupPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/crypto" element={<CryptoPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
